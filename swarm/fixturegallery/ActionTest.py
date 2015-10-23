a02f8dc31091837f0e1c041ee6786b6ef4948c5f
waiverson <237476161@qq.com>
2015-10-23 10:12:29 +0800

    add get token func

--- swarm/fixturegallery/ActionTest.py
+++ swarm/fixturegallery/ActionTest.py
@@ -1,5 +1,5 @@
 # encoding:utf-8
-__author__ = 'abear'
+__author__ = 'xyc'
 
 
 from fit.Fixture import Fixture
@@ -16,15 +16,20 @@ class ActionTest(Fixture):
         self.__url = ""
         self.__headers = None
         self.__params = None
+        self.__data = None
         self.status = ""
         self.result = ""
 
     _typeDict["url"] = "String"
     _typeDict["headers"] = "Dict"
     _typeDict["params"] = "Dict"
+    _typeDict["data"] = "Dict"
     _typeDict["get"] = "Default"
     _typeDict["post"] = "Default"
+    _typeDict["put"] = "Default"
     _typeDict["get_token"] = "Default"
+    _typeDict["post_json"] = "Default"
+    _typeDict["get_detail"] = "Default"
     _typeDict["status"] = "Int"
     _typeDict["result"] = "String"
 
@@ -37,6 +42,9 @@ class ActionTest(Fixture):
     def params(self, s):
         self.__params = dict(s)
 
+    def data(self, s):
+        self.__data = dict(s)
+
     def result(self):
         return self.result
 
@@ -44,7 +52,7 @@ class ActionTest(Fixture):
         return self.status
 
     def get_token(self):
-        r = requests.post(self.__url, self.__params)
+        r = requests.post(self.__url, data=self.__data)
         self.status = r.status_code
         try:
             self.result = json.loads(r.text)['result']['token']
@@ -52,18 +60,40 @@ class ActionTest(Fixture):
         except:
             self.result = r.text
 
+    def get_detail(self):
+        if ActionTest._typeDict.has_key("token"):
+            self.params({'token': ActionTest._typeDict["token"]})
+        r = requests.get(self.__url, params=self.__params)
+        self.status = r.status_code
+        self.result = r.text
+
+    def set_token(self):
+        if ActionTest._typeDict.has_key("token") and isinstance(self.__headers,dict) and self.__headers.has_key('SESSION-TOKEN'):
+            self.__headers['SESSION-TOKEN'] = ActionTest._typeDict["token"]
+
     def get(self):
-        self.__headers['SESSION-TOKEN'] = ActionTest._typeDict["token"]
+        self.set_token()
         r = requests.get(self.__url, params=self.__params, headers=self.__headers)
         self.status = r.status_code
         self.result = r.text
 
     def post(self):
-        self.__headers['SESSION-TOKEN'] = ActionTest._typeDict["token"]
-        r = requests.post(self.__url, params=self.__params, headers=self.__headers)
+        self.set_token()
+        r = requests.post(self.__url, params=self.__params, headers=self.__headers, data=self.__data)
         self.status = r.status_code
         self.result = r.text[:100]
 
+    def post_json(self):
+        self.set_token()
+        r = requests.post(self.__url, params=self.__params, headers=self.__headers, data=json.dumps(self.__data))
+        self.status = r.status_code
+        self.result = r.text[:100]
+
+    def put(self):
+        self.set_token()
+        r = requests.put(self.__url, params=self.__params, headers=self.__headers)
+        self.status = r.status_code
+        self.result = r.text[:100]
 
 
 
