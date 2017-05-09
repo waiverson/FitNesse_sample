5405576498f32e74163d2ba39c203df261474dd3
waiverson <237476161@qq.com>
2017-05-09 12:03:42 +0800

    add path finder using objectpath

--- swarm/core/variables.py
+++ swarm/core/variables.py
@@ -46,14 +46,24 @@ class Variables(object):
         else:
             return self.dsl_path_finder(expr, replacement)
 
-    #objectpath查找，objectpath表达式：%$.result.list[@.name is "SATA 1"].predicted[0]% or '%$.result.total%'
     def object_path_finder(self, object_path_expr, replacement):
+        """
+            基于objectpath查找
+            :param object_path_expr: %$.result.list[@.name is "SATA 1"].predicted[0]% ,'%$.result.total%'
+            :param replacement: dict,string,json,tuple
+            :return:elements生成器, element
+        """
         tree = objectpath.Tree(replacement)
         g = tree.execute(object_path_expr)
         return g
 
-    #dsl查找，dsl表达式：%result@industry_id% 或者 %result@list[n]@industry_id%
     def dsl_path_finder(self,dsl_path_expr, replacement):
+        """
+            基于dsl查找
+            :param object_path_expr: %result@industry_id% , %result@list[n]@industry_id%
+            :param replacement: dict,string,json,tuple
+            :return:elements生成器, element
+        """
         replacement = recursive_json_loads(replacement)
         if "@" in dsl_path_expr:
             slot_values = eval("replacement.{key}".format(self = self,
@@ -62,8 +72,8 @@ class Variables(object):
         else:
             return str(replacement[dsl_path_expr])
 
-    # 替换字符串类型的variable中的“路径查找表达式”
     def substitute_for_str(self, variable):
+        # 替换字符串类型的variable中的“路径查找表达式”
         if self.check(variable) and self.replacement:
             match_expr = Variables.VARIABLES_PATTERN.findall(variable)
             actual_text = variable.replace("%", "")
@@ -73,8 +83,8 @@ class Variables(object):
             return actual_text
         return variable
 
-    # 替换字典类型的variable中的“路径查找表达式”
     def substitute_for_dict(self, variable):
+        # 替换字典类型的variable中的“路径查找表达式”
         for k in variable.keys():
             if self.check(variable[k]) and self.replacement:
                 match_expr = Variables.VARIABLES_PATTERN.findall(variable[k])
@@ -84,35 +94,6 @@ class Variables(object):
                     #variable.update({k:variable[k].replace(expr, v)})
                     variable.update({k:v})
         return variable
-
-
-    # def substitute_for_str_stub(self, variable):
-    #     if self.check(variable) and self.replacement:
-    #         match = Variables.VARIABLES_PATTERN.findall(variable)
-    #         text = variable.replace("%", "")
-    #         for m in match:
-    #             if "@" in m:
-    #                 v = eval("self.replacement.{key}".format(self = self,
-    #                                                    key=m.replace("@",".")))
-    #                 text = text.replace(m, str(v))
-    #             else:
-    #                 text =text.replace(m, str(self.replacement[m]))
-    #         return text
-    #     return variable
-    #
-    # def substitute_for_dict_stub(self, variable):
-    #     for k in variable.keys():
-    #         if self.check(variable[k]) and self.replacement:
-    #             match = Variables.VARIABLES_PATTERN.findall(variable[k])
-    #             variable.update({k:variable[k].replace("%", "")})
-    #             for m in match:
-    #                 if "@" in m:
-    #                     v = eval("self.replacement.{key}".format(self = self,
-    #                                                        key=m.replace("@",".")))
-    #                     variable.update({k:variable[k].replace(m, str(v))})
-    #                 else:
-    #                     variable.update({k:variable[k].replace(m,str(self.replacement[m]))})
-    #     return variable
 
     def check(self, text):
         if isinstance(text, str) and "%" in text:
